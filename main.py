aca7c6ede563c38bbc1ea952ecaa11361e1bab11
Zerae69 <89207779375g@gmail.com>
2026-05-23 17:39:53 +0300

    Удаление книг

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+def delete_book():
+    books = load_books()
 
+    title = input("Название книги для удаления: ")
+
+    new_books = []
+    for b in books:
+        if b["title"] != title:
+            new_books.append(b)
+
+    save_books(new_books)
+    print("Удалено")
