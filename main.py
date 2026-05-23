4c6de225ad332aa4e8f9515f1d92f792d2ea2896
Zerae69 <89207779375g@gmail.com>
2026-05-23 16:21:15 +0300

    Создан каркас программы

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+import json
 
+FILE = "books.json"
+
+def load_books():
+    with open(FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def save_books(books):
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(books, f, ensure_ascii=False, indent=2)
+
+def main():
+    while True:
+        print("1. Добавить книгу")
+        print("2. Показать все книги")
+        print("3. Средняя оценка")
+        print("4. Статистика по авторам")
+        print("5. Удалить книгу")
+        print("6. Выход")
+
+        choice = input("Выбор: ")
+
+        if choice == "6":
+            break
+
+if __name__ == "__main__":
+    main()
